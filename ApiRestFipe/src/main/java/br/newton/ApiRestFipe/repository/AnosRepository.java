909c0132278c262c090115955a807ef1ccd832af
Rafael-Russo <57966987+Rafael-Russo@users.noreply.github.com>
2024-04-19 20:26:32 -0300

    Delete ApiRestFipe directory

--- ApiRestFipe/src/main/java/br/newton/ApiRestFipe/repository/AnosRepository.java
+++
@@ -1,9 +0,0 @@
-package br.newton.ApiRestFipe.repository;
-
-import br.newton.ApiRestFipe.model.AnosEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnosRepository extends MongoRepository<AnosEntity, String> {
-}
--- ApiRestFipe/src/main/java/br/newton/ApiRestFipe/repository/MarcaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.newton.ApiRestFipe.repository;
-
-import br.newton.ApiRestFipe.model.MarcaEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarcaRepository extends MongoRepository<MarcaEntity, String> {
-}
